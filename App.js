164c506750ce6d3896b51bfa484cfccb6e9078a6
Manisha Choudhary <manisha_choudhary@hotmail.com>
2020-05-05 15:51:12 -0400

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+
 import Main from './components/MainComponent';
 
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { DISHES } from '../shared/dishes';
+import { createStackNavigator } from 'react-navigation';
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    
-      this.state = {
-        dishes: DISHES,
-        selectedDish: null
-      };
-}
+const MenuNavigator = createStackNavigator({
+        Menu: { screen: Menu },
+        Dishdetail: { screen: Dishdetail }
+    },
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }
+    }
+);
 
-onDishSelect(dishId) {
-    this.setState({selectedDish: dishId})
-}
+class Main extends Component { 
 
   render() {
  
     return (
-        <View>
-        <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-    </View>
+        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+            <MenuNavigator />
+        </View>
+    //     <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+    //         <MenuNavigator />
+    //     {/* <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+    //     <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
+    // </View>
     );
   }
 }
