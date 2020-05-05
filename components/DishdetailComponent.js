3aab254eb147661f4c96d48df061b1c928998c44
Manisha Choudhary <manisha_choudhary@hotmail.com>
2020-05-04 23:43:35 -0400

    React Native Components Part 2

---
+++ components/DishdetailComponent.js
@@ -0,0 +1,60 @@
+// import React from 'react';
+// import { View, Text } from 'react-native';
+// import { Card } from 'react-native-elements';
+
+
+// function RenderDish(props) {
+//     const dish = props.dish;
+
+//     if(Dish != null) {
+//         return (
+//             <Card
+//             featuredTitle={dish.name}
+//             image={require('./images/uthappizza.png')}>
+//                   <Text style={{margin: 10}}>
+//                   {dish.description}
+//                    </Text>
+//             </Card>
+//         );
+//     }
+//         else {
+//             return (
+//                 <View></View>
+//             );
+//         }
+//     }
+    
+// function Dishdetail(props) {
+//           return (<RenderDish dish={props.dish} />);
+// }
+// export default Dishdetail;
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+function RenderDish(props) {
+
+    const dish = props.dish;
+    
+        if (dish != null) {
+            return(
+                <Card
+                featuredTitle={dish.name}
+                image={require('./images/uthappizza.png')}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
+
+function Dishdetail(props) {
+    return(<RenderDish dish={props.dish} />);
+}
+
+export default Dishdetail;
