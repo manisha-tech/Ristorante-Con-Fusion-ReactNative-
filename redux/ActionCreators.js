fd6fc0ee88c7118ef5b2d28f6a4920f63904d79d
Manisha Choudhary <manisha_choudhary@hotmail.com>
2020-05-26 15:07:59 -0400

    Deleting Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -182,3 +182,9 @@ export const addFavorite = (dishId) => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: dishId
 });
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});
+
--- redux/favorites.js
+++ redux/favorites.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import FavoriteComponent from '../components/FavoriteComponent';
 
 export const favorites = (state = [], action) => {
     switch (action.type) {
@@ -7,6 +8,10 @@ export const favorites = (state = [], action) => {
                 return state;
             else
                 return state.concat(action.payload);
+
+
+        case ActionTypes.DELETE_FAVORITE:
+            return state.filter((favorite) => favorite !== action.payload);
                 
         default:
           return state;
